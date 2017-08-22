2d0e6065c08e93ba413fc955cba25f35fc5177e6
zhangxiahui <281055769@qq.com>
2017-08-22 10:42:48 +0800

    #权限系统  菜单权限和按钮权限开发

--- src/main/java/com/trasen/permission/controller/AuthorizeController.java
+++ src/main/java/com/trasen/permission/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.trasen.permission.common.VisitInfoHolder;
 import com.trasen.permission.model.MenuVo;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,8 +25,8 @@ public class AuthorizeController {
 
     @ResponseBody
     @RequestMapping(value = "/menus", method = RequestMethod.GET)
-    public Result getId() {
-        logger.info("===权限系统====["+ VisitInfoHolder.getUserId()+"]====");
+    public Result getMenus() {
+        logger.info("===权限系统:获取菜单权限====["+ VisitInfoHolder.getUserId()+"]====");
         Result result = new Result();
         result.setMessage("查询成功");
         result.setStatusCode(1);
@@ -61,6 +62,46 @@ public class AuthorizeController {
         menuVo1.setChildrens(list1);
 
         list.add(menuVo1);
+
+        result.setObject(list);
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/operList/{state}", method = RequestMethod.GET)
+    public Result getOperList(@PathVariable String state) {
+        logger.info("===权限系统:获取页面操作权限====["+ VisitInfoHolder.getUserId()+"]====["+state+"]===");
+        Result result = new Result();
+        result.setMessage("查询成功");
+        result.setStatusCode(1);
+        result.setSuccess(true);
+        List<MenuVo> list = new ArrayList<>();
+        MenuVo menuVo1 = new MenuVo();
+        menuVo1.setPkid(1);
+        menuVo1.setName("列表展示");
+        menuVo1.setOpCode("aaa");
+
+
+        MenuVo menuVo2 = new MenuVo();
+        menuVo2.setPkid(2);
+        menuVo2.setName("查询");
+        menuVo2.setOpCode("app.note");
+
+        MenuVo menuVo3 = new MenuVo();
+        menuVo3.setPkid(3);
+        menuVo3.setName("导出");
+        menuVo3.setOpCode("app.attenceList");
+
+        MenuVo menuVo4 = new MenuVo();
+        menuVo4.setPkid(4);
+        menuVo4.setName("删除");
+        menuVo4.setOpCode("app.contact");
+
+
+        list.add(menuVo1);
+        list.add(menuVo2);
+        list.add(menuVo3);
+        list.add(menuVo4);
 
         result.setObject(list);
         return result;
